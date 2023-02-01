fa51e4e2e9b86de73734cdd186a9f7986cf13e47
Siddhesh <sbarapatre7494@gmail.com>
2023-02-02 00:24:11 +0530

    by Ujwala

---
+++ GitProjectChetan/src/com/cjc/day/xyz.java
@@ -0,0 +1,13 @@
+package com.cjc.day;
+
+public class xyz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to mecury page");
+		System.out.println("firstly register");
+		
+		
+	}
+
+}
